3170187328edf5ccab8e0b0c74ac68d752cc01d0
Bayo202 <39320803+Bayo202@users.noreply.github.com>
2022-03-05 15:06:19 -0600

    Added "alpha version" text

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 9, // This changes how many tries you get to finish the wordle
-  language: 'Yorùbá', // This changes the display name for your language
+  language: 'Yorùbá (alpha version)', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Báyọ̀', // Put your name here so people know who made this Wordle!
   authorWebsite: 'https://www.google.com/?hl=yo', // Put a link to your website or social media here
