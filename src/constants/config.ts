c1e85118ed99a83dea2076b6cc9bfca55b340cf3
Bayo202 <39320803+Bayo202@users.noreply.github.com>
2022-03-05 22:51:54 -0600

    Updated config + started translation

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
   tries: 9, // This changes how many tries you get to finish the wordle
-  language: 'Yorùbá (alpha version)', // This changes the display name for your language
+  language: 'Yorùbá (alpha version with tones)', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Báyọ̀', // Put your name here so people know who made this Wordle!
   authorWebsite: 'https://www.google.com/?hl=yo', // Put a link to your website or social media here
-  wordListSource: 'Jẹ́ K\'Á Sọ Yorùbá', // Describe the source material for your words here
+  wordListSource: 'Jẹ́ K\'Á Sọ Yorùbá àti Jẹ́ K\'Á Ka Yorùbá', // Describe the source material for your words here
   wordListSourceLink: 'https://yalebooks.yale.edu/book/9780300055900/je-ka-so-yoruba', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
@@ -12,7 +12,8 @@ export const CONFIG = {
   googleAnalytics: '', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
-  startDate: 'March 4, 2022 17:00:00', // what date and time to start your game from
+  startDate: 'March 5, 2022 17:00:00', // what date and time to start your game from
   defaultLang: 'en', // the default interface language
-  availableLangs: ['en', 'yo'], // the options available to the user for translation languages
+  // TODO: Add Yoruba ("yo")
+  availableLangs: ['en'], // the options available to the user for translation languages
 }
